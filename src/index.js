b271b47045afa4e355e9d09a2b924eded1645e27
Tom Personal <tom@Toms-MacBook-Pro-2.local>
2017-12-29 11:09:03 +0000

    Section 14

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import SearchBar from './components/search_bar'
 
 const API_KEY = 'AIzaSyDCqi-DQAB0kj9u3Pg_ug9VYqDW4OHwEPY';
 
 const App = () => {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
