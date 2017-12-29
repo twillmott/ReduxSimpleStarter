18ce24cdcf2d21ed04951436d4f5cbb43cafecec
Tom Personal <tom@Toms-MacBook-Pro-2.local>
2017-12-29 11:31:52 +0000

    Section 17 - writing value of search bar to state

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,8 +2,18 @@ import React, { Component } from 'react'
 
 class SearchBar extends Component {
 
+  constructor(props){
+    super(props);
+
+    this.state = { term: '' }
+  }
+
   render () {
-    return <input />;
+    return (
+      <div>
+        <input onChange={event => this.setState({ term: event.target.value })} />
+      </div>
+    );
   }
 }
 
