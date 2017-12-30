8afb7c36b7024c7d5bc176bb56adadababe55a5b
Tom Personal <tom@Toms-MacBook-Pro-2.local>
2017-12-30 11:28:06 +0000

    Up to section 28

--- src/index.js
+++ src/index.js
@@ -1,15 +1,33 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import SearchBar from './components/search_bar'
+import VideoList from './components/video_list'
+import VideoDetail from './components/video_detail'
+import YTSearch from 'youtube-api-search'
 
 const API_KEY = 'AIzaSyDCqi-DQAB0kj9u3Pg_ug9VYqDW4OHwEPY';
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+
+constructor(props) {
+  super(props);
+
+  this.state = { videos: [] };
+
+  YTSearch({key: API_KEY, term: 'Surfing'}, (videos) => {
+    this.setState({ videos });
+  });
+}
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.videos[0]}/>
+        <VideoList videos={this.state.videos}/>
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
