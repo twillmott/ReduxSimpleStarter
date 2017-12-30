5301bf538e32eb7beac2759db92c7134571ba386
Tom Personal <tom@Toms-MacBook-Pro-2.local>
2017-12-30 12:28:00 +0000

    End of youtube application (I think?) - Section 32

--- src/index.js
+++ src/index.js
@@ -1,34 +1,41 @@
-import React, { Component } from 'react'
-import ReactDOM from 'react-dom'
-import SearchBar from './components/search_bar'
-import VideoList from './components/video_list'
-import VideoDetail from './components/video_detail'
-import YTSearch from 'youtube-api-search'
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import _ from 'lodash';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+import YTSearch from 'youtube-api-search';
 
 const API_KEY = 'AIzaSyDCqi-DQAB0kj9u3Pg_ug9VYqDW4OHwEPY';
 
 class App extends Component {
 
-constructor(props) {
-  super(props);
+  constructor(props) {
+    super(props);
 
-  this.state = {
-    videos: [],
-    selectedVideo: null
-  };
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+    this.videoSearch("Surfing");
+  }
 
-  YTSearch({key: API_KEY, term: 'Surfing'}, (videos) => {
-    this.setState({
-      videos: videos ,
-      selectedVideo: videos[0]
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
+      this.setState({
+        videos: videos ,
+        selectedVideo: videos[0]
+      });
     });
-  });
-}
+  }
 
   render() {
+
+    const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
+
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={videoSearch} />
         <VideoDetail video={this.state.selectedVideo}/>
         <VideoList
           onVideoSelect={selectedVideo => this.setState({selectedVideo})}
